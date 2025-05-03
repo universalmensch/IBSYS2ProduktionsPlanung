ddb821959180399359a33316a8e86fec6574c4f3
Lukas <melu1025@h-ka.de>
2025-05-03 14:51:57 +0200

    change href to react router dom

--- ibsys2Produktionsplanung/src/components/navbar.tsx
+++ ibsys2Produktionsplanung/src/components/navbar.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'; // <-- import Link
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
-      <a className="navbar-brand mx-3" href="/">SupplyChainManagement</a>
+      <Link className="navbar-brand mx-3" to="/">SupplyChainManagement</Link>
 
       <div className="d-flex">
-        <a className="navbar-item text-decoration-none text-dark px-1" href="/Produktionsplanung">Produktionsplanung</a>
-        <a className="navbar-item text-decoration-none text-dark px-1" href="/Minutenplanung">Minutenplanung</a>
-        <a className="navbar-item text-decoration-none text-dark px-1" href="/KaufteilDisposition">KaufteilDisposition</a>
+        <Link className="navbar-item text-decoration-none text-dark px-1" to="/Produktionsplanung">Produktionsplanung</Link>
+        <Link className="navbar-item text-decoration-none text-dark px-1" to="/Minutenplanung">Minutenplanung</Link>
+        <Link className="navbar-item text-decoration-none text-dark px-1" to="/KaufteilDisposition">KaufteilDisposition</Link>
       </div>
     </nav>
   );
