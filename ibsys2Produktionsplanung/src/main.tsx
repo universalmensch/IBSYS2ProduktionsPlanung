0c7a761f84428606dfe135486e5536058ae39ac9
Konstantin Kraus <krausko@outlook.de>
2025-05-11 10:40:41 +0200

    fix: use different file as production planning

--- ibsys2Produktionsplanung/src/main.tsx
+++ ibsys2Produktionsplanung/src/main.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Startseite } from './pages/Startseite';
-import { Produktionsplanung } from './pages/Produktionsplanung';
-import { Minutenplanung } from './pages/MinutenPlanung';
-import { Kaufteildisposition } from './pages/Kaufteildisposition';
-import { GeneralStoreProvider } from './helper/GeneralStoreContext'; // Import the provider
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Startseite} from './pages/Startseite';
+import {Produktionsplanung} from './pages/Produktionsplanung';
+import {Minutenplanung} from './pages/MinutenPlanung';
+import {Kaufteildisposition} from './pages/Kaufteildisposition';
+import {GeneralStoreProvider} from './helper/GeneralStoreContext'; // Import the provider
 import Navbar from './components/navbar';
 import './i18n.tsx';
+import {TeileProduktion} from "./pages/TeileProduktion.tsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-    <GeneralStoreProvider>
-    <Router>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Startseite />}></Route>
-        <Route path="/Produktionsplanung" element={<Produktionsplanung />}></Route>
-        <Route path="/Minutenplanung" element={<Minutenplanung />}></Route>
-        <Route path="/Kaufteildisposition" element={<Kaufteildisposition />}></Route>
-      </Routes>
-    </Router>
-    </GeneralStoreProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GeneralStoreProvider>
+            <Router>
+                <Navbar></Navbar>
+                <Routes>
+                    <Route path="/" element={<Startseite/>}></Route>
+                    <Route path="/Produktionsplanung" element={<Produktionsplanung/>}></Route>
+                    <Route path="/TeileProduktion" element={<TeileProduktion/>}></Route>
+                    <Route path="/Minutenplanung" element={<Minutenplanung/>}></Route>
+                    <Route path="/Kaufteildisposition" element={<Kaufteildisposition/>}></Route>
+                </Routes>
+            </Router>
+        </GeneralStoreProvider>
+    </React.StrictMode>
 );
