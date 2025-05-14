07f14a8c7717ec8d78be755000a0ed507056385f
Konstantin <krko1012@h-ka.de>
2025-05-15 01:09:20 +0200

    feat: komplette funktionierende Berechnung der Teile Produktion

--- ibsys2Produktionsplanung/src/pages/TeileProduktion.tsx
+++ ibsys2Produktionsplanung/src/pages/TeileProduktion.tsx
@@ -81,21 +81,33 @@ export function TeileProduktion() {
         teil1.auftraege = Number(verkaufsauftraege.p1);
         teil1.planRestbestand = Number(produktionsPlan.p1ProduktionWoche0) + Number(teil1.restBestand) + Number(teil1.warteschlange) + Number(teil1.bearbeitung) - Number(verkaufsauftraege.p1);
 
-        const teil26 = result[26];
-        teil26.auftraege = teil1.menge
-        teil26.fuerWarteschlangen = teil1.warteschlange
+        const teil261 = new Produktionsteil(261, result[26].planRestbestand / 3);
+        teil261.auftraege = teil1.menge
+        teil261.fuerWarteschlangen = teil1.warteschlange
+        teil261.restBestand = (result[26].restBestand / 3)
+        teil261.warteschlange = result[26].warteschlange / 3
+        teil261.bearbeitung = result[26].bearbeitung / 3
+        result[261] = teil261;
 
         const teil51 = result[51];
         teil51.auftraege = teil1.menge
         teil51.fuerWarteschlangen = teil1.warteschlange
 
-        const teil16 = result[16];
-        teil16.auftraege = teil51.menge
-        teil16.fuerWarteschlangen = teil51.warteschlange
+        const teil161 = new Produktionsteil(161, result[16].planRestbestand / 3);
+        teil161.auftraege = teil51.menge;
+        teil161.fuerWarteschlangen = teil51.warteschlange;
+        teil161.restBestand = result[16].restBestand / 3;
+        teil161.warteschlange = result[16].warteschlange / 3;
+        teil161.bearbeitung = result[16].bearbeitung / 3;
+        result[161] = teil161;
 
-        const teil17 = result[17];
-        teil17.auftraege = teil51.menge
-        teil17.fuerWarteschlangen = teil51.warteschlange
+        const teil171 = new Produktionsteil(171, result[17].planRestbestand / 3);
+        teil171.auftraege = teil51.menge;
+        teil171.fuerWarteschlangen = teil51.warteschlange;
+        teil171.restBestand = result[17].restBestand / 3;
+        teil171.warteschlange = result[17].warteschlange / 3;
+        teil171.bearbeitung = result[17].bearbeitung / 3;
+        result[171] = teil171;
 
         const teil50 = result[50];
         teil50.auftraege = teil51.menge
@@ -131,9 +143,33 @@ export function TeileProduktion() {
         teil2.auftraege = Number(verkaufsauftraege.p2);
         teil2.planRestbestand = Number(produktionsPlan.p2ProduktionWoche0) + Number(teil2.restBestand) + Number(teil2.warteschlange) + Number(teil2.bearbeitung) - Number(verkaufsauftraege.p2);
 
+        const teil262 = new Produktionsteil(262, result[26].planRestbestand / 3);
+        teil262.auftraege = teil2.menge;
+        teil262.fuerWarteschlangen = teil2.warteschlange;
+        teil262.restBestand = result[26].restBestand / 3;
+        teil262.warteschlange = result[26].warteschlange / 3;
+        teil262.bearbeitung = result[26].bearbeitung / 3;
+        result[262] = teil262;
+
         const teil56 = result[56];
         teil56.auftraege = teil2.menge
         teil56.fuerWarteschlangen = teil2.warteschlange
+
+        const teil162 = new Produktionsteil(162, result[16].planRestbestand / 3);
+        teil162.auftraege = teil56.menge;
+        teil162.fuerWarteschlangen = teil56.warteschlange;
+        teil162.restBestand = result[16].restBestand / 3;
+        teil162.warteschlange = result[16].warteschlange / 3;
+        teil162.bearbeitung = result[16].bearbeitung / 3;
+        result[162] = teil162;
+
+        const teil172 = new Produktionsteil(172, result[17].planRestbestand / 3);
+        teil172.auftraege = teil56.menge;
+        teil172.fuerWarteschlangen = teil56.warteschlange;
+        teil172.restBestand = result[17].restBestand / 3;
+        teil172.warteschlange = result[17].warteschlange / 3;
+        teil172.bearbeitung = result[17].bearbeitung / 3;
+        result[172] = teil172;
 
         const teil55 = result[55];
         teil55.auftraege = teil56.menge
@@ -168,9 +204,33 @@ export function TeileProduktion() {
         teil3.auftraege = Number(verkaufsauftraege.p3);
         teil3.planRestbestand = Number(produktionsPlan.p3ProduktionWoche0) + Number(teil3.restBestand) + Number(teil3.warteschlange) + Number(teil3.bearbeitung) - Number(verkaufsauftraege.p3);
 
+        const teil263 = new Produktionsteil(263, result[26].planRestbestand / 3);
+        teil263.auftraege = teil3.menge;
+        teil263.fuerWarteschlangen = teil3.warteschlange;
+        teil263.restBestand = result[26].restBestand / 3;
+        teil263.warteschlange = result[26].warteschlange / 3;
+        teil263.bearbeitung = result[26].bearbeitung / 3;
+        result[263] = teil263;
+
         const teil31 = result[31];
         teil31.auftraege = teil3.menge
         teil31.fuerWarteschlangen = teil3.warteschlange
+
+        const teil163 = new Produktionsteil(163, result[16].planRestbestand / 3);
+        teil163.auftraege = teil31.menge;
+        teil163.fuerWarteschlangen = teil31.warteschlange;
+        teil163.restBestand = result[16].restBestand / 3;
+        teil163.warteschlange = result[16].warteschlange / 3;
+        teil163.bearbeitung = result[16].bearbeitung / 3;
+        result[163] = teil163;
+
+        const teil173 = new Produktionsteil(173, result[17].planRestbestand / 3);
+        teil173.auftraege = teil31.menge;
+        teil173.fuerWarteschlangen = teil31.warteschlange;
+        teil173.restBestand = result[17].restBestand / 3;
+        teil173.warteschlange = result[17].warteschlange / 3;
+        teil173.bearbeitung = result[17].bearbeitung / 3;
+        result[173] = teil173;
 
         const teil30 = result[30];
         teil30.auftraege = teil31.menge
@@ -218,143 +278,54 @@ export function TeileProduktion() {
     }
 
     function save() {
+        const preferedOrder = [
+            1, 2, 3,
+            261, 161, 171, 13, 19, 15, 14,
+            262, 20, 18, 10, 11, 12,
+            263, 162, 172,
+            7, 8, 9, 49, 54, 29,
+            4, 5, 6, 50, 55, 30, 51, 56, 31,
+            163, 173
+        ];
+
         const production = produktionsteile
-            .filter(auftrag => auftrag.menge > 0)
+            .filter(auftrag => auftrag.menge > 0 && ![16, 17, 26].includes(auftrag.id))
+            .sort((a, b) => preferedOrder.indexOf(a.id) - preferedOrder.indexOf(b.id))
             .map(auftrag =>
-                new ProduktionsAuftragDTO(auftrag.id, auftrag.menge)
+                new ProduktionsAuftragDTO(getOriginalId(auftrag.id), auftrag.menge)
             )
+
 
         setGeneralStoreData({
             ...generalStore,
             produktionsAuftrag: production
         });
+
+
     }
 
-    function getMenge(id: number) {
-        if (id === 1) {
-            return produktionsPlan.p1ProduktionWoche0
-        }
-        if (id === 2) {
-            return produktionsPlan.p2ProduktionWoche0
-        }
-        if (id === 3) {
-            return produktionsPlan.p3ProduktionWoche0
-        }
-
-        if (id === 26 || id === 16 || id === 17) {
-            return (produktionsteile[id].menge / 3).toFixed(2);
-        }
-
-        return produktionsteile[id].menge;
+    function formatZahl(id: number, wert: number): string {
+        const idsDieGerundetWerden = [161, 162, 163, 171, 172, 173, 261, 262, 263];
+        return idsDieGerundetWerden.includes(id) ? wert.toFixed(2) : String(wert);
     }
 
-    function getRestbestand(id: number) {
-        if (id === 26 || id === 16 || id === 17) {
-            return (produktionsteile[id].restBestand / 3).toFixed(2);
+    function getOriginalId(id: number): number {
+        switch (id) {
+            case 161:
+            case 162:
+            case 163:
+                return 16;
+            case 171:
+            case 172:
+            case 173:
+                return 17;
+            case 261:
+            case 262:
+            case 263:
+                return 26;
+            default:
+                return id;
         }
-        return produktionsteile[id].restBestand;
-    }
-
-    function getWarteschlangen(id: number) {
-        if (id === 26 || id === 16 || id === 17) {
-            return (produktionsteile[id].warteschlange / 3).toFixed(2);
-        }
-        return produktionsteile[id].warteschlange;
-    }
-
-    function getBearbeitung(id: number) {
-        if (id === 26 || id === 16 || id === 17) {
-            return (produktionsteile[id].bearbeitung / 3).toFixed(2);
-        }
-        return produktionsteile[id].bearbeitung;
-    }
-
-    function getAuftraege(key: number) {
-        const keyString = key.toString();
-        if (keyString.length > 2) {
-            switch (keyString[0] + keyString[1]) {
-                case '16' :
-                case '17' : {
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[51].menge;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[56].menge;
-                    }
-                    return produktionsteile[31].menge;
-                }
-                case '26' :
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsPlan.p1ProduktionWoche0;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsPlan.p2ProduktionWoche0;
-                    }
-                    return produktionsPlan.p3ProduktionWoche0;
-                default:
-                    return 0;
-            }
-        }
-        return produktionsteile[key].auftraege;
-    }
-
-    function getFuerWarteschlangen(key: number) {
-        const keyString = key.toString();
-        if (keyString.length > 2) {
-            switch (keyString[0] + keyString[1]) {
-                case '16' :
-                case '17' : {
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[51].warteschlange;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[56].warteschlange;
-                    }
-                    return produktionsteile[31].warteschlange;
-                }
-                case '26' :
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[1].warteschlange;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[2].warteschlange;
-                    }
-                    return produktionsteile[3].warteschlange;
-                default:
-                    return 0;
-            }
-        }
-        return produktionsteile[key].fuerWarteschlangen;
-    }
-
-    function getPlanRestbestand(key: number) {
-        const keyString = key.toString();
-        if (keyString.length > 2) {
-            switch (keyString[0] + keyString[1]) {
-                case '16' : {
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[16].planRestbestand;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[16].planRestbestand;
-                    }
-                    return produktionsteile[16].planRestbestand;
-                }
-                case '17' : {
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[17].planRestbestand;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[17].planRestbestand;
-                    }
-                    return produktionsteile[17].planRestbestand;
-                }
-                case '26' :
-                    if (Number(keyString[2]) === 1) {
-                        return produktionsteile[26].planRestbestand;
-                    } else if (Number(keyString[2]) === 2) {
-                        return produktionsteile[26].planRestbestand;
-                    }
-                    return produktionsteile[26].planRestbestand;
-                default:
-                    return 0;
-            }
-        }
-        return produktionsteile[key].planRestbestand;
     }
 
     return (
@@ -365,45 +336,44 @@ export function TeileProduktion() {
                 <tr>
                     <th>Produktionsteil</th>
                     <th>Aufträge</th>
-                    <th>+ Aus Warteschlangen</th>
-                    <th>+ Plan Restbestand</th>
-                    <th>- Restbestand</th>
-                    <th>- Warteschlange</th>
-                    <th>- In Bearbeitung</th>
+                    <th>Aus Warteschlangen</th>
+                    <th>Plan Restbestand</th>
+                    <th>Restbestand</th>
+                    <th>Warteschlange</th>
+                    <th>In Bearbeitung</th>
                     <th>Produktionsmenge</th>
                 </tr>
                 </thead>
                 <tbody>
                 {
                     AnzeigeReihenfolge.map(
-                        key => {
-                            const id = key.toString().length > 2 ? Number(key.toString()[0] + key.toString()[1]) : key;
+                        id => {
                             return <>
-                                <tr key={key + 'vorne'} style={{border: 'none'}}>{
+                                <tr key={id + 'vorne'} style={{border: 'none'}}>{
                                     //extra space between the different bikes
                                     (id === 2 || id === 3) &&
                                     <td colSpan={2} style={{padding: 0, height: '15px', border: 'none'}}/>
 
                                 }</tr>
-                                <tr key={key + 'mitte'}>
-                                    <td>{produktionsteile[id].id}</td>
-                                    <td>{getAuftraege(key)}</td>
-                                    <td>{getFuerWarteschlangen(key)}</td>
+                                <tr key={id + 'mitte'}>
+                                    <td>{getOriginalId(id)}</td>
+                                    <td>{produktionsteile[id].auftraege}</td>
+                                    <td>+ {produktionsteile[id].fuerWarteschlangen}</td>
                                     <td>
-                                        <input
-                                            min={0}
-                                            disabled={id === 1 || id === 2 || id === 3}
-                                            type="number"
-                                            value={getPlanRestbestand(key)}
-                                            onChange={(e) => setAuftrag(id, Number(e.target.value))}
-                                        />
+                                        + <input
+                                        min={0}
+                                        disabled={id === 1 || id === 2 || id === 3}
+                                        type="number"
+                                        value={produktionsteile[id].planRestbestand}
+                                        onChange={(e) => setAuftrag(id, Number(e.target.value))}
+                                    />
                                     </td>
-                                    <td>{getRestbestand(id)}</td>
-                                    <td>{getWarteschlangen(id)}</td>
-                                    <td>{getBearbeitung(id)}</td>
-                                    <td>{getMenge(id)}</td>
+                                    <td>- {formatZahl(id, produktionsteile[id].restBestand)}</td>
+                                    <td>- {formatZahl(id, produktionsteile[id].warteschlange)}</td>
+                                    <td>- {formatZahl(id, produktionsteile[id].bearbeitung)}</td>
+                                    <td>{formatZahl(id, produktionsteile[id].menge)}</td>
                                 </tr>
-                                <tr key={key + 'hinten'} style={{border: 'none'}}>{
+                                <tr key={id + 'hinten'} style={{border: 'none'}}>{
                                     //extra spaces for the structure of the table
                                     (SpaceAfterRow.includes(id)) &&
                                     <td colSpan={2} style={{padding: 0, height: '45px', border: 'none'}}/>
