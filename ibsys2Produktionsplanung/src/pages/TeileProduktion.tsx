bb74277481dfdd0843c1b402d92784248c730113
Konstantin <krko1012@h-ka.de>
2025-05-14 23:11:54 +0200

    feat: vorläufige behandlung von dreifachen teilen

--- ibsys2Produktionsplanung/src/pages/TeileProduktion.tsx
+++ ibsys2Produktionsplanung/src/pages/TeileProduktion.tsx
@@ -13,6 +13,7 @@ export function TeileProduktion() {
     const restBestand = input?.warehousestock?.article
     const waitingWorkplace = input?.waitinglistworkstations.workplace || []
     const inBearbeitung = input?.ordersinwork?.workplace
+    const verkaufsauftraege = input?.forecast || {p1: 0, p2: 0, p3: 0}
 
     const produktionsPlan = generalStore?.produktionsPlan ?? new ProduktionsPlanDTO(0, 0, 0);
 
@@ -77,7 +78,8 @@ export function TeileProduktion() {
 
         // P1
         const teil1 = result[1];
-        teil1.auftraege = produktionsPlan.p1ProduktionWoche0
+        teil1.auftraege = Number(verkaufsauftraege.p1);
+        teil1.planRestbestand = Number(produktionsPlan.p1ProduktionWoche0) + Number(teil1.restBestand) + Number(teil1.warteschlange) + Number(teil1.bearbeitung) - Number(verkaufsauftraege.p1);
 
         const teil26 = result[26];
         teil26.auftraege = teil1.menge
@@ -126,7 +128,8 @@ export function TeileProduktion() {
 
         // P2
         const teil2 = result[2];
-        teil2.auftraege = produktionsPlan.p2ProduktionWoche0
+        teil2.auftraege = Number(verkaufsauftraege.p2);
+        teil2.planRestbestand = Number(produktionsPlan.p2ProduktionWoche0) + Number(teil2.restBestand) + Number(teil2.warteschlange) + Number(teil2.bearbeitung) - Number(verkaufsauftraege.p2);
 
         const teil56 = result[56];
         teil56.auftraege = teil2.menge
@@ -162,7 +165,8 @@ export function TeileProduktion() {
 
         // P2
         const teil3 = result[3];
-        teil3.auftraege = produktionsPlan.p3ProduktionWoche0
+        teil3.auftraege = Number(verkaufsauftraege.p3);
+        teil3.planRestbestand = Number(produktionsPlan.p3ProduktionWoche0) + Number(teil3.restBestand) + Number(teil3.warteschlange) + Number(teil3.bearbeitung) - Number(verkaufsauftraege.p3);
 
         const teil31 = result[31];
         teil31.auftraege = teil3.menge
@@ -227,65 +231,182 @@ export function TeileProduktion() {
     }
 
     function getMenge(id: number) {
-        if (id === 26 || id === 16 || id === 17) {
-            return produktionsteile[id]?.menge / 3;
+        if (id === 1) {
+            return produktionsPlan.p1ProduktionWoche0
         }
-        return produktionsteile[id]?.menge;
+        if (id === 2) {
+            return produktionsPlan.p2ProduktionWoche0
+        }
+        if (id === 3) {
+            return produktionsPlan.p3ProduktionWoche0
+        }
+
+        if (id === 26 || id === 16 || id === 17) {
+            return (produktionsteile[id].menge / 3).toFixed(2);
+        }
+
+        return produktionsteile[id].menge;
     }
 
     function getRestbestand(id: number) {
         if (id === 26 || id === 16 || id === 17) {
-            return produktionsteile[id]?.restBestand / 3;
+            return (produktionsteile[id].restBestand / 3).toFixed(2);
         }
-        return produktionsteile[id]?.restBestand;
+        return produktionsteile[id].restBestand;
+    }
+
+    function getWarteschlangen(id: number) {
+        if (id === 26 || id === 16 || id === 17) {
+            return (produktionsteile[id].warteschlange / 3).toFixed(2);
+        }
+        return produktionsteile[id].warteschlange;
+    }
+
+    function getBearbeitung(id: number) {
+        if (id === 26 || id === 16 || id === 17) {
+            return (produktionsteile[id].bearbeitung / 3).toFixed(2);
+        }
+        return produktionsteile[id].bearbeitung;
+    }
+
+    function getAuftraege(key: number) {
+        const keyString = key.toString();
+        if (keyString.length > 2) {
+            switch (keyString[0] + keyString[1]) {
+                case '16' :
+                case '17' : {
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[51].menge;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[56].menge;
+                    }
+                    return produktionsteile[31].menge;
+                }
+                case '26' :
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsPlan.p1ProduktionWoche0;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsPlan.p2ProduktionWoche0;
+                    }
+                    return produktionsPlan.p3ProduktionWoche0;
+                default:
+                    return 0;
+            }
+        }
+        return produktionsteile[key].auftraege;
+    }
+
+    function getFuerWarteschlangen(key: number) {
+        const keyString = key.toString();
+        if (keyString.length > 2) {
+            switch (keyString[0] + keyString[1]) {
+                case '16' :
+                case '17' : {
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[51].warteschlange;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[56].warteschlange;
+                    }
+                    return produktionsteile[31].warteschlange;
+                }
+                case '26' :
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[1].warteschlange;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[2].warteschlange;
+                    }
+                    return produktionsteile[3].warteschlange;
+                default:
+                    return 0;
+            }
+        }
+        return produktionsteile[key].fuerWarteschlangen;
+    }
+
+    function getPlanRestbestand(key: number) {
+        const keyString = key.toString();
+        if (keyString.length > 2) {
+            switch (keyString[0] + keyString[1]) {
+                case '16' : {
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[16].planRestbestand;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[16].planRestbestand;
+                    }
+                    return produktionsteile[16].planRestbestand;
+                }
+                case '17' : {
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[17].planRestbestand;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[17].planRestbestand;
+                    }
+                    return produktionsteile[17].planRestbestand;
+                }
+                case '26' :
+                    if (Number(keyString[2]) === 1) {
+                        return produktionsteile[26].planRestbestand;
+                    } else if (Number(keyString[2]) === 2) {
+                        return produktionsteile[26].planRestbestand;
+                    }
+                    return produktionsteile[26].planRestbestand;
+                default:
+                    return 0;
+            }
+        }
+        return produktionsteile[key].planRestbestand;
     }
 
     return (
         <div>
-            <h1>Produktionsplanung</h1>
+            <h1>Teileproduktion</h1>
             <Table>
                 <thead>
                 <tr>
                     <th>Produktionsteil</th>
                     <th>Aufträge</th>
-                    <th>Aus Warteschlangen</th>
-                    <th>Plan Restbestand</th>
-                    <th>Restbestand</th>
-                    <th>Warteschlange</th>
-                    <th>In Bearbeitung</th>
-                    <th>Menge</th>
+                    <th>+ Aus Warteschlangen</th>
+                    <th>+ Plan Restbestand</th>
+                    <th>- Restbestand</th>
+                    <th>- Warteschlange</th>
+                    <th>- In Bearbeitung</th>
+                    <th>Produktionsmenge</th>
                 </tr>
                 </thead>
                 <tbody>
                 {
                     AnzeigeReihenfolge.map(
-                        id => {
+                        key => {
+                            const id = key.toString().length > 2 ? Number(key.toString()[0] + key.toString()[1]) : key;
                             return <>
-                                <tr key={Math.random()}>{
+                                <tr key={key + 'vorne'} style={{border: 'none'}}>{
                                     //extra space between the different bikes
-                                    (id === 2 || id === 3) && <></>
+                                    (id === 2 || id === 3) &&
+                                    <td colSpan={2} style={{padding: 0, height: '15px', border: 'none'}}/>
 
                                 }</tr>
-                                <tr key={Math.random()}>
-                                    <td>{produktionsteile[id]?.id}</td>
-                                    <td>{produktionsteile[id]?.auftraege}</td>
-                                    <td>{produktionsteile[id]?.fuerWarteschlangen}</td>
+                                <tr key={key + 'mitte'}>
+                                    <td>{produktionsteile[id].id}</td>
+                                    <td>{getAuftraege(key)}</td>
+                                    <td>{getFuerWarteschlangen(key)}</td>
                                     <td>
                                         <input
                                             min={0}
+                                            disabled={id === 1 || id === 2 || id === 3}
                                             type="number"
-                                            value={produktionsteile[id]?.planRestbestand}
+                                            value={getPlanRestbestand(key)}
                                             onChange={(e) => setAuftrag(id, Number(e.target.value))}
                                         />
                                     </td>
                                     <td>{getRestbestand(id)}</td>
-                                    <td>{produktionsteile[id]?.warteschlange}</td>
-                                    <td>{produktionsteile[id]?.bearbeitung}</td>
+                                    <td>{getWarteschlangen(id)}</td>
+                                    <td>{getBearbeitung(id)}</td>
                                     <td>{getMenge(id)}</td>
                                 </tr>
-                                <tr key={Math.random()}>{
+                                <tr key={key + 'hinten'} style={{border: 'none'}}>{
                                     //extra spaces for the structure of the table
-                                    (SpaceAfterRow.includes(id)) && <></>
+                                    (SpaceAfterRow.includes(id)) &&
+                                    <td colSpan={2} style={{padding: 0, height: '45px', border: 'none'}}/>
 
                                 }</tr>
                             </>;
