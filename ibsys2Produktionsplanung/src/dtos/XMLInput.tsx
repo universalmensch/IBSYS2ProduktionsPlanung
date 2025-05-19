149782829e553a1ffd57b3f0ae3aafad480d3f76
Lukas Merkle <lme@objektkultur.de>
2025-05-19 13:51:27 +0200

    Changes so it works

--- ibsys2Produktionsplanung/src/dtos/XMLInput.tsx
+++ ibsys2Produktionsplanung/src/dtos/XMLInput.tsx
@@ -1,164 +1,164 @@
 export type XMLInput = {
-    results: {
-        period: number;
-        forecast: { p1: number, p2: number, p3: number };
-        warehousestock: WarehouseStock;
-        inwardstockmovement: InwardStockMovement;
-        futureinwardstockmovement: FutureInwardStockMovement;
-        idletimecosts: IdleTimeCosts;
-        waitinglistworkstations: WaitingListWorkstations;
-        waitinglistdtock: WaitingListStock;
-        ordersinwork: OrdersInWork;
-        completedorders: CompletedOrders;
-        cycletimes: CycleTimes;
-    }
+  results: {
+    period: number;
+    forecast: { p1: number; p2: number; p3: number };
+    warehousestock: WarehouseStock;
+    inwardstockmovement: InwardStockMovement;
+    futureinwardstockmovement: FutureInwardStockMovement;
+    idletimecosts: IdleTimeCosts;
+    waitinglistworkstations: WaitingListWorkstations;
+    waitinglistdtock: WaitingListStock;
+    ordersinwork: OrdersInWork;
+    completedorders: CompletedOrders;
+    cycletimes: CycleTimes;
+  };
 };
 type WarehouseStock = {
-    article: Article[];
-    totalstockvalue: number;
+  article: Article[];
+  totalstockvalue: number;
 };
 
 type Article = {
-    id: number;
-    amount: number;
-    startamount: number;
-    pct: number;
-    price: number;
-    stockvalue: number;
+  id: number;
+  amount: number;
+  startamount: number;
+  pct: number;
+  price: number;
+  stockvalue: number;
 };
 
 type InwardStockMovement = {
-    order: Order[];
+  order: Order[];
 };
 
 type Order = {
-    orderperiod: number;
-    id: number;
-    mode: number;
-    article: number;
-    amount: number;
-    time: number;
-    materialcosts: number;
-    ordercosts: number;
-    entirecosts: number;
-    piececosts: number;
+  orderperiod: number;
+  id: number;
+  mode: number;
+  article: number;
+  amount: number;
+  time: number;
+  materialcosts: number;
+  ordercosts: number;
+  entirecosts: number;
+  piececosts: number;
 };
 
 type FutureInwardStockMovement = {
-    order: FutureOrder[];
+  order: FutureOrder[];
 };
 
 type FutureOrder = {
-    orderperiod: number;
-    id: number;
-    mode: number;
-    article: number;
-    amount: number;
+  orderperiod: number;
+  id: number;
+  mode: number;
+  article: number;
+  amount: number;
 };
 
 type IdleTimeCosts = {
-    workplace: Workplace[];
-    sum: IdleTimeSum;
+  workplace: Workplace[];
+  sum: IdleTimeSum;
 };
 
 type Workplace = {
-    id: number;
-    setupevents: number;
-    idletime: number;
-    wageidletimecosts: number;
-    wagecosts: number;
-    machineidletimecosts: number;
+  id: number;
+  setupevents: number;
+  idletime: number;
+  wageidletimecosts: number;
+  wagecosts: number;
+  machineidletimecosts: number;
 };
 
 type IdleTimeSum = {
-    setupevents: number;
-    idletime: number;
-    wageidletimecosts: number;
-    wagecosts: number;
-    machineidletimecosts: number;
+  setupevents: number;
+  idletime: number;
+  wageidletimecosts: number;
+  wagecosts: number;
+  machineidletimecosts: number;
 };
 
 type WaitingListWorkstations = {
-    workplace: WaitingWorkplace[];
+  workplace: WaitingWorkplace[];
 };
 
 type WaitingWorkplace = {
-    id: number;
-    timeneed: number;
-    waitinglist?: WaitingList[];
+  id: number;
+  timeneed: number;
+  waitinglist?: WaitingList[];
 };
 
 type WaitingList = {
-    period: number;
-    order: number;
-    firstbatch: number;
-    lastbatch: number;
-    item: number;
-    amount: number;
-    timeneed: number;
+  period: number;
+  order: number;
+  firstbatch: number;
+  lastbatch: number;
+  item: number;
+  amount: number;
+  timeneed: number;
 };
 
 type WaitingListStock = {
-    missingpart: MissingPart[];
+  missingpart: MissingPart[];
 };
 
 type MissingPart = {
-    id: number;
-    workplace: MissingWorkplace[];
+  id: number;
+  workplace: MissingWorkplace[];
 };
 
 type MissingWorkplace = {
-    id: number;
-    timeneed: number;
-    waitinglist: WaitingList[];
+  id: number;
+  timeneed: number;
+  waitinglist: WaitingList[];
 };
 
 type OrdersInWork = {
-    workplace: OrdersWorkplace[];
+  workplace: OrdersWorkplace[] | OrdersWorkplace;
 };
 
 type OrdersWorkplace = {
-    id: number;
-    period: number;
-    order: number;
-    batch: number;
-    item: number;
-    amount: number;
-    timeNeed: number;
+  id: number;
+  period: number;
+  order: number;
+  batch: number;
+  item: number;
+  amount: number;
+  timeNeed: number;
 };
 
 type CompletedOrders = {
-    order: CompletedOrder[];
+  order: CompletedOrder[];
 };
 
 type CompletedOrder = {
-    period: number;
-    id: number;
-    item: number;
-    quantity: number;
-    cost: number;
-    averageunitcosts: number;
-    batch: Batch[];
+  period: number;
+  id: number;
+  item: number;
+  quantity: number;
+  cost: number;
+  averageunitcosts: number;
+  batch: Batch[];
 };
 
 type Batch = {
-    id: number;
-    amount: number;
-    cycletime: number;
-    cost: number;
+  id: number;
+  amount: number;
+  cycletime: number;
+  cost: number;
 };
 
 type CycleTimes = {
-    startedorders: number;
-    waitingorders: number;
-    order: CycleTimeOrder[];
+  startedorders: number;
+  waitingorders: number;
+  order: CycleTimeOrder[];
 };
 
 type CycleTimeOrder = {
-    id: number;
-    period: number;
-    starttime: string;
-    finishtime: string;
-    cycletimemin: number;
-    cycletimefactor: number;
+  id: number;
+  period: number;
+  starttime: string;
+  finishtime: string;
+  cycletimemin: number;
+  cycletimefactor: number;
 };
